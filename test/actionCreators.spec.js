78cbe56aaf051a6ccfc8c8aec7b78a2e7ee3b1ba
Cole Willsea <coleww@gmail.com>
2016-09-05 12:54:54 -0700

    actionCreator tests might be overkill in this app? leaving them as good example code

--- test/actionCreators.spec.js
+++ test/actionCreators.spec.js
@@ -2,6 +2,9 @@ import expect from 'expect'
 import * as actions from '../client/actions/actionCreators'
 
 describe('actions', () => {
+  // TODO: none of the actionCreators in this app are complex enough to merit testing them all
+  // leaving these as an example, but it would be better to do the 'action types' business i think?
+
   it('should create an action to increment a players score', () => {
     const expectedAction = {
       type: 'INCREMENT_SCORE',
@@ -9,5 +12,14 @@ describe('actions', () => {
       amount: 100
     }
     expect(actions.incrementScore(1, 100)).toEqual(expectedAction)
+  })
+
+  it('should create an action to decrement a players score', () => {
+    const expectedAction = {
+      type: 'DECREMENT_SCORE',
+      playerId: 1,
+      amount: 100
+    }
+    expect(actions.decrementScore(1, 100)).toEqual(expectedAction)
   })
 })
