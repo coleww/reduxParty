d1fdd90b55f98177977fca60bc1ff2b2348388bc
Cole Willsea <coleww@gmail.com>
2016-09-11 10:44:02 -0700

    update welcome page

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -8,8 +8,15 @@ const Welcome = React.createClass({
   render() {
     return (
       <div className="welcome-message">
-        WELCOME 2 JEOPARDY!
+        WELCOME TO JEOPARDY!
+
+        <br />
+        Use one browser tab for the "host" to see answers and assign points, and another tab to display the board to the players and audience. Data is synced between the two via localStorage.
+
+        <br />
         <Link to="/game">Start a new game!</Link>
+        <br/><br/>
+        built with React and Redux, by <a href="http://www.colewillsea.com">Cole Willsea</a>. (<a href="https://github.com/coleww/reduxparty">source code</a>)
       </div>
     );
   }
